b2e3d1bd5847e0ea60abc57248a53d18380d2044
Benny <bennymanuel2020@gmail.com>
2026-08-22 23:24:40 +0530

    chore: configure firebase-admin as external package and disable reactStrictMode

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  serverExternalPackages: ["firebase-admin"],
+  reactStrictMode: false,
+}
 
-export default nextConfig;
+export default nextConfig
